f837d5ef963ec47d95298a7ea9f4f98bfab2fb67
Kam Parsen <kamparsen@yahoo.com>
2020-10-24 16:38:20 -0400

    removed extra boiler plate code not needed

--- stackout/src/index.js
+++ stackout/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify, { Auth } from 'aws-amplify';
+import aws_exports from './aws-exports';
+
+import "@aws-amplify/ui/dist/style.css";
+Amplify.configure(aws_exports);
 
 ReactDOM.render(
   <React.StrictMode>
